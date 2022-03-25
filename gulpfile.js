13e2b6012e8d135d315d150a4312eb3ee7e0c604
Umangkumar Patel <umangkumar.18beceg031@gmail.com>
2022-03-25 15:08:53 +0530

    Gulp part 1

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+gulp.task('sass', function() {
+    return gulp.src('./css/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve', gulp.series('sass', function() {
+	var files = [
+      './*.html',
+      './css/*.css',
+      './img/*.{png,jpg,gif}',
+      './js/*.js'
+   ];
+
+   browserSync.init(files, {
+      server: {
+         baseDir: "./"
+      }
+   });
+
+    gulp.watch('./css/*.scss', gulp.series('sass'));
+}));
+
+gulp.task('default', gulp.series('serve'));
